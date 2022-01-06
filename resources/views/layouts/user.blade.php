fcb6060397d1c6c650c44ceb66183c2f5f8bc713
GokeAdewuyi <yusufadewuyi2@gmail.com>
2022-01-06 19:43:58 +0100

    update

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -891,6 +891,18 @@
 </script>
 
 @yield('script')
+
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/61d7335eb84f7301d329b6f4/1fooa1bol';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
 </body>
 
 </html>
