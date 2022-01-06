e2e7578813a9f4bbdaa533cb87c42c6de30dec3b
GokeAdewuyi <yusufadewuyi2@gmail.com>
2022-01-06 19:23:26 +0100

    update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -37,6 +37,34 @@
         crossorigin="anonymous"></script>
     <script src="js/plugins.js"></script>
     <script src="js/theme.js"></script>
+
+{{--    <!--Start of Tawk.to Script-->--}}
+{{--    <script type="text/javascript">--}}
+{{--        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();--}}
+{{--        (function(){--}}
+{{--            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];--}}
+{{--            s1.async=true;--}}
+{{--            s1.src='https://embed.tawk.to/61d72c31b84f7301d329b5b4/1foo899cg';--}}
+{{--            s1.charset='UTF-8';--}}
+{{--            s1.setAttribute('crossorigin','*');--}}
+{{--            s0.parentNode.insertBefore(s1,s0);--}}
+{{--        })();--}}
+{{--    </script>--}}
+{{--    <!--End of Tawk.to Script-->--}}
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/61d7335eb84f7301d329b6f4/1fooa1bol';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
