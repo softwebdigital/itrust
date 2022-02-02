bb0d03b4f440db70693892e0b313ff5c8be1bfc7
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-02-02 20:42:48 +0100

    update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="{{ asset('card.jpeg') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
     <title>Itrust Investments</title>
     <link rel="stylesheet" href="css/plugins.css">
