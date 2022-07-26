93a5418e7921c0482838f80c4c7d644c6380801d
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-07-26 15:13:31 +0100

    update og image

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:image" content="{{ asset('card.png') }}">
+    <meta property="og:image" content="{{ asset('card.jpeg') }}">
+    <meta property="og:title" content="Itrust Investments - Investing in Stocks, Crypto Trading, Online broker and Research tools you need.">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
     <title>Itrust Investments</title>
     <link rel="stylesheet" href="css/plugins.css">
