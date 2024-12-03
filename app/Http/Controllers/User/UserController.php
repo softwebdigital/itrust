4e4d4b216707bf139bc6b6715e329fe5bb3934e6
Sanni David <sannidavidsmart@gmail.com>
2024-12-03 22:26:48 +0100

    fix a user 226

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -317,21 +317,21 @@ class UserController extends Controller
 
         //::::: FIX A USERS WALLET VALUE  :::::
 
-        $last_iraValue = end($iraData);
+        // $last_iraValue = end($iraData);
 
-        if($user->id == '226') {
-            $wallet = [
-                'balance' => $totalValue,
-                'ic_wallet' => 0,
-                'it_wallet' => $last_iraValue, // $user->calculateBalances()['ira_trading'],
-                'oc_wallet' => $user->calculateBalances()['offshore_cash'],
-                'ot_wallet' => $user->calculateBalances()['offshore_trading'],
-            ];
+        // if($user->id == '226') {
+        //     $wallet = [
+        //         'balance' => $totalValue,
+        //         'ic_wallet' => $user->calculateBalances()['ira_cash'],
+        //         'it_wallet' => $last_iraValue, // $user->calculateBalances()['ira_trading'],
+        //         'oc_wallet' => $user->calculateBalances()['offshore_cash'],
+        //         'ot_wallet' => $user->calculateBalances()['offshore_trading'],
+        //     ];
 
-            $user->createOrUpdateWallet($wallet);
+        //     $user->createOrUpdateWallet($wallet);
 
-            // dd(end($iraData));
-        }
+        //     // dd(end($iraData));
+        // }
         // dd(end($iraData));
 
         //::::: FIX A USERS WALLET VALUE  :::::
