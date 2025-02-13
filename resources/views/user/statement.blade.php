841f471556f739041a5d8ac8b95e30d5cada1c5b
Sanni David <sannidavidsmart@gmail.com>
2025-02-13 10:17:03 +0100

    feat: fix T

--- resources/views/user/statement.blade.php
+++ resources/views/user/statement.blade.php
@@ -29,7 +29,6 @@
             <table id="datatable" class="table table-borderless table-responsive  nowrap w-100">
                 <thead>
                 <tr>
-                    <th>Copy Bots</th>
                     <th>Account</th>
                     <th>Investment</th>
                     <th>Asset</th>
@@ -45,26 +44,6 @@
                 <tbody>
                @foreach($investments as $investment)
                    <tr>
-                       <td>
-                       @if($investment->copyBot)
-                            <div class="col">
-                                <img src="{{ $investment->copyBot->image }}" alt="" width="50" class="rounded-circle">
-                            </div>
-                        @elseif($investment->type == 'Cryptocurrencies')
-                            <div class="col">
-                                <img src="https://itrustinvestment.com/img/bots/170457095116.png" alt="" width="50" class="rounded-circle">
-                            </div>
-                        @elseif($investment->type == 'stocks')
-                            <div class="col">
-                                <img src="https://www.itrustinvestment.com/img/bots/1701947098355_2021-06-22-14-19-40.png" alt="" width="50" class="rounded-circle">
-                            </div>
-                        @else 
-                            <div class="col">
-                                <img src="https://www.itrustinvestment.com/img/bots/1687558439CCAC78C9-717D-4CE9-AB03-88C2814C6A65.jpeg" alt="" width="50" class="rounded-circle">
-                            </div>
-                        @endif
-
-                       </td>
                        <td>
                             @if($investment->acct_type == 'offshore')
                                 Offshore
@@ -97,7 +76,8 @@
                                 }
                             @endphp
                             <h5>{{ $symbol->symbol }}{{ number_format($investment->ROI, 2) }}</h5>
-                            <p class="text-success">+{{ number_format($percentage) }} %</p>
+                            <p class="@if($percentage < 0) text-danger @else text-success @endif"> @if($percentage > 0) + @endif
+                                {{ number_format($percentage) }} %</p>
                         </td>
                        <td>
                             {{ ucwords($investment->leverage) }}X
